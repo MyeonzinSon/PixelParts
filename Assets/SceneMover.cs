459036fe9c7131b32da89b5f11e4f588114af6de
DESKTOP-F4T27T3\user <cyhs91209120@gmail.com>
2018-08-18 16:01:48 +0900

    ㅁㄴㅇㄹ

--- Assets/SceneMover.cs
+++ Assets/SceneMover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneMover : MonoBehaviour {
 
@@ -13,4 +14,9 @@ public class SceneMover : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void MoveScene()
+    {
+        SceneManager.LoadScene("In game scene")
+    }
 }
