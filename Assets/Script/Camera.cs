9e12e5a5dc29495ec65f7251158b293383d4262e
Myeongzin Son <sonmyungzin1@naver.com>
2018-08-31 14:51:35 +0900

    여러가지 계속 작업 중

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
     GameObject player;
-    
+    public float horizontalLimit = 39;
+    public float verticalLimit = 0.4f;
     void Start () {
 	    player = GameManager.Instance.playerGO;
     }
     void Update () {
         float x = player.transform.position.x;
-        if (x > 32) {
-            x = 32;
+        if (x > horizontalLimit) {
+            x = horizontalLimit;
         }
-        if (x < -32) {
-            x = -32;
+        if (x < -horizontalLimit) {
+            x = -horizontalLimit;
         }
         float y = player.transform.position.y;
-        if (y > 0.4) {
-            y = 0.4f;
+        if (y > verticalLimit) {
+            y = verticalLimit;
         }
-        if (y < -0.4) {
-            y = -0.4f;
+        if (y < -verticalLimit) {
+            y = -verticalLimit;
         }
         transform.position = new Vector3(x, y, transform.position.z);
     }
